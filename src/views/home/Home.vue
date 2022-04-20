3e4517c2d9ce26d4d872b2e87fc6dbdd1ffeaa9f
youthinn <1413106927@qq.com>
2022-04-20 13:13:02 +0800

    首页分类数据完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,121 +4,25 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <featrue-view></featrue-view>
-    <tab-control :titles="['流行','新款','精选']"></tab-control>
-    <ul>
-      <li>李太白1</li>
-      <li>李太白2</li>
-      <li>李太白3</li>
-      <li>李太白4</li>
-      <li>李太白5</li>
-      <li>李太白6</li>
-      <li>李太白7</li>
-      <li>李太白8</li>
-      <li>李太白9</li>
-      <li>李太白10</li>
-      <li>李太白11</li>
-      <li>李太白12</li>
-      <li>李太白13</li>
-      <li>李太白14</li>
-      <li>李太白15</li>
-      <li>李太白16</li>
-      <li>李太白17</li>
-      <li>李太白18</li>
-      <li>李太白19</li>
-      <li>李太白20</li>
-      <li>李太白21</li>
-      <li>李太白22</li>
-      <li>李太白23</li>
-      <li>李太白24</li>
-      <li>李太白25</li>
-      <li>李太白26</li>
-      <li>李太白27</li>
-      <li>李太白28</li>
-      <li>李太白29</li>
-      <li>李太白30</li>
-      <li>李太白31</li>
-      <li>李太白32</li>
-      <li>李太白33</li>
-      <li>李太白34</li>
-      <li>李太白35</li>
-      <li>李太白36</li>
-      <li>李太白37</li>
-      <li>李太白38</li>
-      <li>李太白39</li>
-      <li>李太白40</li>
-      <li>李太白41</li>
-      <li>李太白42</li>
-      <li>李太白43</li>
-      <li>李太白44</li>
-      <li>李太白45</li>
-      <li>李太白46</li>
-      <li>李太白47</li>
-      <li>李太白48</li>
-      <li>李太白49</li>
-      <li>李太白50</li>
-      <li>李太白51</li>
-      <li>李太白52</li>
-      <li>李太白53</li>
-      <li>李太白54</li>
-      <li>李太白55</li>
-      <li>李太白56</li>
-      <li>李太白57</li>
-      <li>李太白58</li>
-      <li>李太白59</li>
-      <li>李太白60</li>
-      <li>李太白61</li>
-      <li>李太白62</li>
-      <li>李太白63</li>
-      <li>李太白64</li>
-      <li>李太白65</li>
-      <li>李太白66</li>
-      <li>李太白67</li>
-      <li>李太白68</li>
-      <li>李太白69</li>
-      <li>李太白70</li>
-      <li>李太白71</li>
-      <li>李太白72</li>
-      <li>李太白73</li>
-      <li>李太白74</li>
-      <li>李太白75</li>
-      <li>李太白76</li>
-      <li>李太白77</li>
-      <li>李太白78</li>
-      <li>李太白79</li>
-      <li>李太白80</li>
-      <li>李太白81</li>
-      <li>李太白82</li>
-      <li>李太白83</li>
-      <li>李太白84</li>
-      <li>李太白85</li>
-      <li>李太白86</li>
-      <li>李太白87</li>
-      <li>李太白88</li>
-      <li>李太白89</li>
-      <li>李太白90</li>
-      <li>李太白91</li>
-      <li>李太白92</li>
-      <li>李太白93</li>
-      <li>李太白94</li>
-      <li>李太白95</li>
-      <li>李太白96</li>
-      <li>李太白97</li>
-      <li>李太白98</li>
-      <li>李太白99</li>
-      <li>李太白100</li>
-    </ul>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="goods[curremtType].list"></goods-list>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
-import TabControl from 'components/content/tabControl/TabControl.vue';
+import TabControl from "components/content/tabControl/TabControl.vue";
+import GoodsList from "components/content/goods/GoodsList.vue";
 
-import HomeSwiper from "./childComponents/HomeSwiper"
-import RecommendView from "./childComponents/RecommendView.vue"
-import FeatrueView from "./childComponents/FeatureView.vue"
+import HomeSwiper from "./childComponents/HomeSwiper";
+import RecommendView from "./childComponents/RecommendView.vue";
+import FeatrueView from "./childComponents/FeatureView.vue";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   components: {
@@ -126,7 +30,8 @@ export default {
     HomeSwiper,
     RecommendView,
     FeatrueView,
-    TabControl
+    TabControl,
+    GoodsList,
   },
 
   Swipername: "Home",
@@ -134,27 +39,68 @@ export default {
     return {
       banners: [],
       recommends: [],
-      goods:{
-        'pop':{page:0,list:[]},
-        'news':{page:0,list:[]},
-        'sell':{page:0,list:[]},
-      }
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      curremtType: "pop",
     };
   },
   created() {
-    // 1.请求多个数据
-    getHomeMultidata().then((res) => {
-      // console.log(res);
-      // this.result = res
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    // 1.请求多个数据  这里使用的是下面methods定义的方法
+    this.getHomeMultidata();
+    // 2.请求商品数据  这里使用的是下面methods定义的方法
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    /**
+     * 事件监听相关的方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.curremtType = "pop";
+          break;
+        case 1:
+          this.curremtType = "new";
+          break;
+        case 2:
+          this.curremtType = "sell";
+          break;
+      }
+    },
+
+    /**
+     *网络请求相关的方法
+     */
+    getHomeMultidata() {
+      //下面使用的getHomeMultidata是从import导入的方法
+      getHomeMultidata().then((res) => {
+        // console.log(res);
+        // this.result = res
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+
+    getHomeGoods(type) {
+      //下面使用的getHomeGoods是从import导入的方法
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        // this.$refs.scroll.finishPullUp()
+      });
+    },
   },
 };
 </script>
 
 <style>
-#home{
+#home {
   padding-top: 44px;
 }
 
